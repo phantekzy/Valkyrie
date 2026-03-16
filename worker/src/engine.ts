c9f7d7aa603df16b23076e681dee93aeb274d021
phantekzy <mainilotfi@gmail.com>
2026-03-16 04:18:24 +0100

    Rebuilding the Worker engine logic

--- worker/src/engine.ts
+++ worker/src/engine.ts
@@ -1,8 +1,18 @@
 import { AxiosInstance } from "axios";
-import {TelemetryEvent} from "../../shared/protocol.js"
+import { TelemetryEvent } from "../../shared/protocol";
+import axios from "axios";
+
 export class LoadEngine {
   private active = false;
   private http: AxiosInstance;
 
-    constructor(private nodeId : string , private onReport :(data :TelemetryEvent ))
+  constructor(
+    private nodeId: string,
+    private onReport: (data: TelemetryEvent) => Promise<void>,
+  ) {
+    this.http = axios.create({
+      timeout: 5000,
+      validateStatus: () => true,
+    });
+  }
 }
